945a110136838dc041a00d4fbc0d755e993ed85c
BoJhong <yangbozhone@gmail.com>
2024-04-04 21:58:34 +0800

    加入專案檔案。

---
+++ PriceCompartor/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PriceCompartor.Models;
+
+namespace PriceCompartor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ PriceCompartor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PriceCompartor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
