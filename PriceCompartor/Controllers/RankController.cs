80aceefce786c57b66de66e242289aba8d19a6ca
BoJhong <yangbozhone@gmail.com>
2024-04-21 08:54:32 +0800

    修正排行榜的排序計算有誤

--- PriceCompartor/Controllers/RankController.cs
+++ PriceCompartor/Controllers/RankController.cs
@@ -26,8 +26,9 @@ namespace PriceCompartor.Controllers
             if (maxSales == 0) maxSales = 1;
             if (maxRatingCount == 0) maxRatingCount = 1;
 
+            // 依照評分、評分人數和銷量進行排序（無人評分則當作2.5分）
             var topProducts = _context.Products
-                .OrderByDescending(p => (p.Rating == 0 ? 3 : p.Rating) / 5 * 0.5 + p.Sales / maxSales * 0.3 + p.TotalRatingCount / maxRatingCount * 0.2) // 依照評分、評分人數和銷量進行排序（無人評分則當作3分）
+                .OrderByDescending(p => (p.Rating == 0.0 ? 2.5 : p.Rating) / 5.0 * 0.5 + p.Sales / (double)maxSales * 0.3 + p.TotalRatingCount / (double)maxRatingCount * 0.2)
                 .Take(10)
                 .ToList();
 
